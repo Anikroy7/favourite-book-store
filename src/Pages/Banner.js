93b27c4acccc54ad2fe92c7dc909e48c991c6e5b
Anikroy7 <roy645800@gmail.com>
2022-07-20 18:37:06 +0600

    banner and navbar added

---
+++ src/Pages/Banner.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import book1 from '../image/banner-image/book-1.jpg'
+import book2 from '../image/banner-image/book-2.webp'
+import book3 from '../image/banner-image/book-cover-mockup_125540-572.webp'
+
+const Banner = () => {
+    return (
+        <div class="carousel w-full h-96">
+            <div id="slide1" class="carousel-item relative w-full">
+                <img src={book1} class="w-full" />
+                <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide4" class="btn btn-circle">❮</a>
+                    <a href="#slide2" class="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide2" class="carousel-item relative w-full">
+                <img src={book2} class="w-full" />
+                <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide1" class="btn btn-circle">❮</a>
+                    <a href="#slide3" class="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide3" class="carousel-item relative w-full">
+                <img src={book3} class="w-full" />
+                <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide2" class="btn btn-circle">❮</a>
+                    <a href="#slide4" class="btn btn-circle">❯</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
